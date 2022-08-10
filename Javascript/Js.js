a47897b2356532351d875f64ece4739873b8c355
fsndiaye <fsndiaye@student.rtc.edu>
2022-08-10 10:36:13 -0700

    End of home session

--- Javascript/Js.js
+++ Javascript/Js.js
@@ -2,3 +2,11 @@
 window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle('window-scrolled', window.scrollY > 0);
 })
+
+//Contact Buttons (circular text buttons)
+const textButtons = document.querySelectorAll('.contact__btn');
+
+textButtons.forEach(textButtons => {
+    let text = textButtons.querySelector('p');
+    text.innerHTML=text.innerHTML.split('').map((character,index) => `<span style="transform: rotate(${index*12}deg">${character}</span>`).join('')
+})
