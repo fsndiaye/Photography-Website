c1664eccd343ce9b915bb7617bf596efd2a853ff
fsndiaye <fsndiaye@student.rtc.edu>
2022-08-20 16:06:21 -0700

    Nav-Toggle - MediaQueries

--- Javascript/Js.js
+++ Javascript/Js.js
@@ -32,3 +32,32 @@ var swiper = new Swiper(".mySwiper", {
         }
     }
   });
+
+
+  //Nav Toggle - open & close
+  const nav = document.querySelector('.nav_links');
+  const openNavBtn = document.querySelector('#nav_toggle-open');
+  const closeNavBtn = document.querySelector('#nav_toggle-close');
+
+  const openNav = () => {
+    nav.style.display = 'flex';
+    openNavBtn.style.display = 'none';
+    closeNavBtn.style.display = 'inline-block';
+  }
+  openNavBtn.addEventListener('click', openNav);
+
+
+  const closeNav = () => {
+    nav.style.display = 'none';
+    openNavBtn.style.display = 'inline block';
+    closeNavBtn.style.display = 'none';
+  }
+  closeNavBtn.addEventListener('click', closeNav);
+  
+
+
+  if(document.body.clientWidth < 1024){
+    nav.querySelectorAll('li a').forEach(navlink => {
+        navlink.addEventListener('click', closeNav);
+      })
+  }
