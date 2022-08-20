9dc635b7cc90fda96cfaeb8d012300707a8d97c9
fsndiaye <fsndiaye@student.rtc.edu>
2022-08-19 22:18:15 -0700

    Gallery  & Exhibitions Part done

--- Javascript/Js.js
+++ Javascript/Js.js
@@ -10,3 +10,25 @@ textButtons.forEach(textButtons => {
     let text = textButtons.querySelector('p');
     text.innerHTML=text.innerHTML.split('').map((character,index) => `<span style="transform: rotate(${index*12}deg">${character}</span>`).join('')
 })
+
+
+
+//Swiper JS
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+        599:{
+            slidesPerView: 2,
+            spaceBetween: 40
+        },
+        1023:{
+            slidesPerView: 3,
+            spaceBetween: 60
+        }
+    }
+  });
